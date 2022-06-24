0a8599132c4cf7a2abb3bdeb18444a3f5d7e09c3
Pedro Sanches <sanches.metaverse@gmail.com>
2022-06-24 18:28:34 -0300

    Integrated with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-    return <div>Hello</div>
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="yellow" p={3} mb={6} align="center">
+            What&apos;s up? I&apos;m a mobile developer specializing in web3!
+            </Box>
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrou={1}>
+                    <Heading as="h2" variant="page-title">
+                        Pedro Sanches
+                    </Heading>
+                    <p>Digital Craftsman ( Developer / 3D Modeler / Designer )</p>
+                </Box>
+            </Box>
+        </Container>
+    )
 }
 
 export default Page
