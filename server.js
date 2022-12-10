712aabd1ed946e62756490cbbd562d11aa121da9
Pedro Sanches <sanches.metaverse@gmail.com>
2022-12-10 01:08:07 -0300

    Fixed server,

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
+const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
 const port = process.env.port || 3000
 const app = next({ dev, hostname, port })
