ff552d46f04726413c609c7e085e485e8965a685
Pedro Sanches <sanches.metaverse@gmail.com>
2023-03-15 00:20:46 -0300

    Fixed import for Image

--- components/logo.js
+++ components/logo.js
@@ -1,43 +1,41 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { Text, useColorModeValue } from '@chakra-ui/react'
+import { Text, Image, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
-    font-weith: bold;
-    fonr-size: 18px;
-    display: inline-flex;
-    align-items: center;
-    height: 30px;
-    line-heigth: 20px;
-    padding: 10px;
+  font-weith: bold;
+  fonr-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-heigth: 20px;
+  padding: 10px;
 
-    &:hover img {
-        transform: rotate(-20deg);
-    }
+  &:hover img {
+    transform: rotate(-20deg);
+  }
 `
 
 const Logo = () => {
-    const avatarImg = `/images/logo${useColorModeValue('', '-dark')}.svg`
+  const avatarImg = `/images/logo${useColorModeValue('', '-dark')}.svg`
 
-    return (
-        <Link href="/">
-            <a>
-                <LogoBox>
-                    <Image src={avatarImg} width={25} height={25} alt="logo" />
-                    <Text
-                    color={useColorModeValue('gray.800', 'whiteAplha.900')}
-                    fontFamily="'M PLUS ROUNDED 1c'"
-                    fontWeight="bold"
-                    ml={3}
-                    >
-                        Pedro Sanches
-                    </Text>
-            
-                </LogoBox>
-            </a>
-        </Link>
-    )
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={avatarImg} width={25} height={25} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAplha.900')}
+            fontFamily="'M PLUS ROUNDED 1c'"
+            fontWeight="bold"
+            ml={3}
+          >
+            Pedro Sanches
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
 }
 
 export default Logo
